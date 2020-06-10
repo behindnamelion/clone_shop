3fd3ca532977a5b6e0edab16569d2567bcc8a3b8
behindnamelion <behindname@likelion.org>
2020-06-10 05:38:23 +0000

    first commit

---
+++ app/admin/packs.rb
@@ -0,0 +1,50 @@
+ActiveAdmin.register Pack do
+  index do
+    selectable_column
+    
+    id_column
+    
+    column :image do |pack|
+      if pack.image.attached?
+        image_tag url_for(pack.image), class: "small-img"
+      else
+        "이미지 없음"
+      end
+    end
+    column :product_name
+    column :company_name
+    column :desc
+    
+    actions
+    
+  end
+  
+  
+  # new, edit 커스텀하기
+  form do |f|
+  	f.inputs do
+  	  f.input :image, as: :file
+  	  f.input :product_name
+  	  f.input :company_name
+  	  f.input :desc
+    end
+  f.actions
+  end
+  
+  # show 부분 customize
+  show do
+    attributes_table do
+      row :id
+      row :image do |pack|
+        if pack.image.attached?
+          image_tag url_for(pack.image), class: "small-img"
+        else
+          "이미지 없음"
+        end
+      end
+      row :product_name
+      row :company_name
+      row :desc
+    end
+  end
+end
