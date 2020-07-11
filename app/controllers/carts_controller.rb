f933bd5070adbb7948adcb32ff592136ae596a48
behindnamelion <behindname@likelion.org>
2020-07-11 03:44:56 +0000

    Change cart part with method and Add ajax delete logic to order

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -21,16 +21,8 @@ class CartsController < ApplicationController
       quantity: params[:quantity]
     )
       
-    sample_carts = current_user.carts
-    present_cart = sample_carts.find_by(pack_id: params[:pack_id])
-
-    if present_cart.present?
-      sum_quantity = present_cart.quantity + params[:quantity].to_i
-      result = present_cart.update(quantity: sum_quantity)
-    else
-      result = cart.save
-    end
-     
+    cart.verified_save
+    
     flash[:notice] = "장바구니에 상품이 담겼습니다.장바구니로 이동하시겠습니까?"
     redirect_back(fallback_location: root_path)
   end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,20 @@
 class Cart < ApplicationRecord
   belongs_to :user
   belongs_to :pack
+  
+  def verified_save
+    sample_carts = self.user.carts
+
+    # 기존에 내가 똑같은 팩을 등록했는지 찾기
+    remain_cart = sample_carts.find_by(pack_id: self.pack_id)
+
+    if remain_cart.present?
+      sum_quantity = remain_cart.quantity + self.quantity.to_i
+
+      remain_cart.update(quantity: sum_quantity)
+    else
+      self.save
+    end
+  end
+  
 end
